b04e43244be127dd79941eac0bfba91233458e09
irinaMenshikh <menshikh26@gmail.com>
2026-03-14 20:34:10 +0200

    add new changes

--- nodeBasics/index.js
+++ nodeBasics/index.js
@@ -1,4 +1,4 @@
 import chalk from 'chalk';
 
 console.log(chalk.blue('Привіт! Це кольоровий текст'));
-console.log(chalk.red.bold('Цей текст червоний та жирний'));
+console.log(chalk.green.bold('Цей текст червоний та жирний'));
