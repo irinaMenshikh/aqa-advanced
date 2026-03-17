5e786e678b4de40428378e91ea484cff6c4f477e
irinaMenshikh <menshikh26@gmail.com>
2026-03-17 21:02:26 +0200

    Added a new branch lesson_jsBasics and completed the tasks for 4.1. JavaScript primitive data types.

---
+++ jsBasics/task4.2.js
@@ -0,0 +1,6 @@
+let length = 6;
+let width = 4;
+
+let result = length * width;
+
+console.log(result);
---
+++ jsBasics/task4.3.js
@@ -0,0 +1,6 @@
+let radius = 12;
+let height = 24;
+
+let result = Math.PI * radius ** 2 * height;
+
+console.log(result.toFixed(2));
