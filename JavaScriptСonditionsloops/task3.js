0b1e8ed7064726fa8c5809574818d5ca3c147233
irinaMenshikh <menshikh26@gmail.com>
2026-03-21 20:35:27 +0200

    Added only JavaScriptConditionsloops

---
+++ JavaScriptСonditionsloops/task3.js
@@ -0,0 +1,10 @@
+let num = 5;
+for(let i = 1; i <= 10; i++) { 
+    console.log(`${num} * ${i} = ${i * num}`); 
+}
+
+let i = 0;
+while (i <= 9) {
+    i++;
+    console.log(`${num} * ${i} = ${i * num}`);
+}
