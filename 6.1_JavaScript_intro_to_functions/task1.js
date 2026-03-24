1bc3a46936f129537062015a72fef015510e84fc
irinaMenshikh <menshikh26@gmail.com>
2026-03-24 20:31:30 +0200

    add lesson 6.1

---
+++ 6.1_JavaScript_intro_to_functions/task1.js
@@ -0,0 +1,13 @@
+function calculateRectangleArea (width, height) { 
+    return width * height; 
+} 
+console.log(calculateRectangleArea(5, 10)); 
+
+const getRectArea = function (width, height) {
+    return width * height; 
+}; 
+
+console.log(getRectArea(5, 10)); 
+
+const getRectangleArea = (width, height) => width * height;  
+console.log(getRectangleArea(5, 10))
